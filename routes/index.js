d3f4be1f4ccacf094961a32a1acd70be06225d97
Javier Ocampo <ferroxido@gmail.com>
2017-08-22 09:46:26 -0300

    Fix routes

--- routes/index.js
+++ routes/index.js
@@ -35,11 +35,11 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
+    app.get('/home/:category?', routes.views.index);
+    app.get('/home/post/:post', routes.views.post);
     app.get('/about-us', routes.views.aboutus);
     app.get('/presentation', routes.views.presentation);
     app.get('/join-us', routes.views.joinus);
-	app.get('/blog/:category?', routes.views.blog);
-	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
 
